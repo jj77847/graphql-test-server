3a2a6841fb3851e052ec60416da98577a5dfd196
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-02 11:30:23 +0000

    schema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-const { gql } = require{'apollo-server'};
+const { gql } = require("apollo-server");
 
 const schema = gql`
+  type Author {
+        firstName: String
+        lastName: String 
+    }
+  
+  type Book {
+    id: String
+    title: String
+    author: Author
+    }
+  }
 
-`
+  type Query {
+      
+    books: [Book]
+    authors: [Author]
+  }
+`;
 
-// 46.31m week 17, day 1 (graphql)
+// (time)m week 17, day 1 (graphql)
